c98c8c639f467f79762fe74d4105a412eb3a61f6
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-12-05 11:12:43 -0600

    enforced lint rules

--- EstimatePi.js
+++ EstimatePi.js
@@ -1,24 +1,25 @@
 /**
- * 
+ *
  * @param {number of random points on an x,y graph between (0,1)} n
- * 
+ *
  * @returns {estimation of PI} Number
  */
 const estimatePi = (n) => {
-    let numberOfPointsWithinCircle = 0;
-    for(let i = 0; i < n; i+=1){
-        if(
-            (
-                Math.sqrt(
-                    (Math.random())**2
-                    + (Math.random()) **2
-                )
-            ) < 1
-        ){
-            numberOfPointsWithinCircle += 1;
-        }
+  let numberOfPointsWithinCircle = 0;
+  for (let i = 0; i < n; i += 1) {
+    if (
+      (
+        Math.sqrt(
+          (Math.random()) ** 2
+                    + (Math.random()) ** 2,
+        )
+      ) < 1
+    ) {
+      numberOfPointsWithinCircle += 1;
     }
-    return 4 * numberOfPointsWithinCircle / n;
+  }
+  return ((4 * numberOfPointsWithinCircle) / n);
 };
 
+// eslint-disable-next-line
 console.log(estimatePi(9999999));
--- InOrderTraverse.js
+++ InOrderTraverse.js
@@ -1,52 +1,53 @@
-const nodeFactory = (value, leftTail, rightTail) => {
-    return {
-        value,
-        leftTail,
-        rightTail,
-    }
-};
+const nodeFactory = (value, leftTail, rightTail) => ({
+  value,
+  leftTail,
+  rightTail,
+});
 
-const _five = nodeFactory(5,null,null);
-const _eleven = nodeFactory(11,null,null);
-const _four = nodeFactory(4,null,null);
-const _two = nodeFactory(2,null,null);
-const _six = nodeFactory(6,_five,_eleven);
-const _nine = nodeFactory(9,_four, null);
-const _seven = nodeFactory(7,_two,_six);
-const _five1 = nodeFactory(5,null,_nine);
-const head = nodeFactory(2,_seven,_five1);
+const nodefive = nodeFactory(5, null, null);
+const nodeeleven = nodeFactory(11, null, null);
+const nodefour = nodeFactory(4, null, null);
+const nodetwo = nodeFactory(2, null, null);
+const nodesix = nodeFactory(6, nodefive, nodeeleven);
+const nodenine = nodeFactory(9, nodefour, null);
+const nodeseven = nodeFactory(7, nodetwo, nodesix);
+const nodefive1 = nodeFactory(5, null, nodenine);
+const head = nodeFactory(2, nodeseven, nodefive1);
 
 /**
  * Prints the binary tree in inorder traversal
- * @param {Head of binary tree} node 
+ * @param {Head of binary tree} node
  */
 const inOrderTraverseRecursive = (node) => {
-    if(node !== null){
-        inOrderTraverseRecursive(node.leftTail);
-        console.log(node.value);
-        inOrderTraverseRecursive(node.rightTail);
-    }
+  if (node !== null) {
+    inOrderTraverseRecursive(node.leftTail);
+    // eslint-disable-next-line
+    console.log(node.value);
+    inOrderTraverseRecursive(node.rightTail);
+  }
 };
 
 /**
  * Prints the binary tree in inorder traversal
- * @param {Head of binary tree} head 
+ * @param {Head of binary tree} head
  */
-const inOrderTraverseIterative = (head) => {
-    const stack = [];
-    let currentNode = head;
-    while(stack.length !== 0 || currentNode !== null){
-        if(currentNode !== null){
-            stack.push(currentNode);
-            currentNode = currentNode.leftTail;
-        } else {
-            currentNode = stack.pop();
-            console.log(currentNode.value);
-            currentNode = currentNode.rightTail;
-        }
+const inOrderTraverseIterative = (headNode) => {
+  const stack = [];
+  let currentNode = headNode;
+  while (stack.length !== 0 || currentNode !== null) {
+    if (currentNode !== null) {
+      stack.push(currentNode);
+      currentNode = currentNode.leftTail;
+    } else {
+      currentNode = stack.pop();
+      // eslint-disable-next-line
+      console.log(currentNode.value);
+      currentNode = currentNode.rightTail;
     }
-}
+  }
+};
 
 inOrderTraverseRecursive(head);
+// eslint-disable-next-line
 console.log('---------------');
 inOrderTraverseIterative(head);
