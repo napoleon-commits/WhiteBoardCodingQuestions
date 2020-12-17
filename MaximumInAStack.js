8dee679e03c0d130483bf6563ec5e92f99a1b61b
Byron Hall <byronhall@MacBook-Pro.attlocal.net>
2020-12-17 14:37:34 -0600

    dec 17

--- MaximumInAStack.js
+++ MaximumInAStack.js
@@ -1,3 +1,9 @@
+/**
+ * Implement a class for a stack that supports all the regular functions
+ * (push, pop) and an additional function of max() which returns the maximum
+ * element in the stack (return None if the stack is empty).
+ * Each method should run in constant time.
+ */
 class MaxStack {
   constructor() {
     this.stack = [];
