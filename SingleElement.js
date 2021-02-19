f378ec0f818a773438d34fca943e16013e1a3fa3
Byron Hall <hall@controlpoint.com>
2021-02-19 13:54:09 -0500

    feb 19

--- SingleElement.js
+++ SingleElement.js
@@ -1,3 +1,33 @@
+function dec2bin(dec) {
+  return dec.toString(2);
+}
+
+function bin2dec(bin) {
+  return parseInt(bin, 2);
+}
+
+function xor(num1, num2) {
+  let binary1 = dec2bin(num1);
+  let binary2 = dec2bin(num2);
+  let longest;
+  if (binary1.length > binary2.length) {
+    longest = binary1.length;
+  } else {
+    longest = binary2.length;
+  }
+  binary1 = binary1.padStart(longest, '0');
+  binary2 = binary2.padStart(longest, '0');
+  let result = '';
+  for (let i = 0; i < longest; i += 1) {
+    if (binary1[i] === binary2[i]) {
+      result += '0';
+    } else {
+      result += '1';
+    }
+  }
+  return bin2dec(result);
+}
+
 /**
  *
  * Given an array of integers, nums, where all numbers occur twice except
@@ -8,24 +38,9 @@
  * @returns {Number}
  */
 function findSingle(nums) {
-  const hashMap = {};
+  let single = 0;
   for (let i = 0; i < nums.length; i += 1) {
-    if (!hashMap[nums[i]]) {
-      hashMap[nums[i]] = 1;
-    } else {
-      hashMap[nums[i]] += 1;
-    }
-  }
-  let foundSingleBool = false;
-  let i = 0;
-  let single;
-  while (foundSingleBool === false && i < Object.keys(hashMap).length) {
-    if (hashMap[Object.keys(hashMap)[i]] === 1) {
-      single = Object.keys(hashMap)[i];
-      foundSingleBool = true;
-    } else {
-      i += 1;
-    }
+    single = xor(single, nums[i]);
   }
   return single;
 }
